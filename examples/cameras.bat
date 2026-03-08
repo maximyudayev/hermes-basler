fc9a31008e25b9fca249169096465dac9609ba33
Maxim Yudayev <maxim.yudayev@gmail.com>
2026-03-08 10:32:35 +0100

    ops: activate venv, use correct config file

--- examples/cameras.bat
+++ examples/cameras.bat
@@ -1,1 +1,3 @@
+@echo on
+call .venv\Scripts\activate
 call hermes-cli -o .\data --config_file cameras.yml --experiment project=Test type=Cameras trial=0
--- examples/cameras.sh
+++ examples/cameras.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+. ../.venv/bin/activate
 hermes-cli -o ./data --config_file cameras.yml --experiment project=Test type=Cameras trial=0
